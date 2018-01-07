993849c565143a46452791c81efdbe210b3aa2f1
Maycon <mayconn.ms@gmail.com>
2018-01-07 21:06:11 -0200

    Changed test method

--- src/main/java/br/com/quantum/controller/Home.java
+++ src/main/java/br/com/quantum/controller/Home.java
@@ -1,10 +1,17 @@
 package br.com.quantum.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 	
 @RestController
 @RequestMapping("/")
 public class Home {
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/test")
+	public ResponseEntity<String> test() throws Exception {
+		return  ResponseEntity.ok("ok");		
+	}
 	
 }
